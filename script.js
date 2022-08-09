5fdd98af1a67a7d8ef948a4ed7aff93f064f1b4b
Isa <Isaismail3000@gmail.com>
2022-08-09 18:50:43 +0300

    Make changes to button text, button placement, and give divs a class

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+const body = document.querySelector('body')
 const container = document.getElementById('grid-container');
 const button = document.createElement('button');
-button.textContent = 'generate grid';
-container.appendChild(button);
+button.textContent = 'generate 16x16 grid';
+body.insertBefore(button, container);
 
 button.addEventListener('click', () => {
    
-    for (i=0; i<=16; i++) {
+    for (i=0; i<256; i++) {
         const div = document.createElement('div');
-        div.textContent = 'meow';
+        div.classList.add('grid-item')
         container.appendChild(div);
     }
 })
