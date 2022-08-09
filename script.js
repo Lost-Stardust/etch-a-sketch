06de0c97f5e1c7b60e11b859547ab25766810e52
Isa <Isaismail3000@gmail.com>
2022-08-09 18:24:56 +0300

    Add event listener to the button to create divs for the grid

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const container = document.getElementById('grid-container');
 const button = document.createElement('button');
 button.textContent = 'generate grid';
 container.appendChild(button);
+
+button.addEventListener('click', () => {
+   
+    for (i=0; i<=16; i++) {
+        const div = document.createElement('div');
+        div.textContent = 'meow';
+        container.appendChild(div);
+    }
+})
