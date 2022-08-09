96330404c68c807a434ee92760367f9fcb0d839d
Isa <Isaismail3000@gmail.com>
2022-08-09 21:53:18 +0300

    Add an evenListener to each grid item that changes backgroundColor on hover
    comment out code for the button to generate a grid for now, add in use
    strict

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+"use strict";
+
 const body = document.querySelector('body')
 const container = document.getElementById('grid-container');
 const button = document.createElement('button');
 button.textContent = 'generate 16x16 grid';
 body.insertBefore(button, container);
 
-button.addEventListener('click', () => {
+// button.addEventListener('click', () => {
    
-    for (i=0; i<256; i++) {
-        const div = document.createElement('div');
-        div.classList.add('grid-item')
-        container.appendChild(div);
-    }
-})
+//     for (i=0; i<256; i++) {
+//         const div = document.createElement('div');
+//         div.classList.add('grid-item')
+//         container.appendChild(div);
+//     }
+// })
+for (let i=0; i<256; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid-item')
+    container.appendChild(div);
+}
+
+// gridItem is an array type html collection
+const gridItem = document.getElementsByClassName('grid-item');
+console.log(gridItem);
+
+for (let i=0; i<gridItem.length; i++) {
+    gridItem[i].addEventListener('mouseover', () => {
+        gridItem[i].style.backgroundColor = 'red';
+    })
+}
