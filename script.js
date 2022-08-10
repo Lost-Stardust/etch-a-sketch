32d8e7b3707feb6bbfae7e5d2bd73bf61673ca99
Isa <Isaismail3000@gmail.com>
2022-08-10 17:21:23 +0300

    Add button that will show modal on click

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 "use strict";
 
+const overlay = document.getElementById('overlay')
+const modal = document.getElementById('modal')
 const body = document.querySelector('body')
 const container = document.getElementById('grid-container');
 const button = document.createElement('button');
 button.textContent = 'Change grid size';
 body.insertBefore(button, container);
 
-// button.addEventListener('click', () => {
-   
-//     for (i=0; i<256; i++) {
-//         const div = document.createElement('div');
-//         div.classList.add('grid-item')
-//         container.appendChild(div);
-//     }
-// })
+button.addEventListener('click', () => {
+   overlay.classList.add('active');
+   modal.classList.add('active');
+})
 for (let i=0; i<256; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-item')
